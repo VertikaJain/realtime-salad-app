b5fbe27b6624d1f7730cfee1b8ec3469ced71d63
Vertika Jain <vertikajain96@gmail.com>
2020-08-24 21:43:30 +0530

    Added Login & Register page #Day60

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ app.get('/cart', (req, res) => {
     res.render("customers/cart");
 })
 
+app.get('/login', (req, res) => {
+    res.render("auth/login");
+})
+
+app.get('/register', (req, res) => {
+    res.render("auth/register");
+})
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
