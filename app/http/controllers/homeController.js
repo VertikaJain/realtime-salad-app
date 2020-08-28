b553dc8fcd26fe48b3a1ea1e1adf59e94ec8af96
Vertika Jain <vertikajain96@gmail.com>
2020-08-28 20:15:46 +0530

    Menu Schema & Model with Dynamic Menu on Home Page #Day62

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,7 +1,12 @@
+const Menu = require("../../models/menu");
 homeController = () => { //factory function
     return {
-        index(req, res) { //index: function(){ }
-            res.render("home");
+        async index(req, res) { //index: function(){ }
+            const salads = await Menu.find();
+            res.render("home", { salads }); // can be written as {salads: salads}
+            /* Menu.find().then(salads => {
+                res.render("home", { salads: salads });
+            }) */
         }
     }
 }
---
+++ app/models/menu.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; // Schema class
+
+const menuSchema = new Schema({ // constructor
+    name: { type: String, required: true },
+    image: { type: String, required: true }, // stored in form of url
+    price: { type: Number, required: true },
+    size: { type: String, required: true },
+});
+
+// creating model
+const MenuModel = mongoose.model("Menu", menuSchema);
+module.exports = MenuModel;
