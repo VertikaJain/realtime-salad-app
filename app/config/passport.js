ff9589d2858c2d485792dc16e30521692fc77244
Vertika Jain <vertikajain96@gmail.com>
2020-09-15 23:41:22 +0530

    Passport library config for Authentication at Login #Day71

---
+++ app/config/passport.js
@@ -0,0 +1,36 @@
+const { models } = require("mongoose");
+
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+init = passport => {
+    passport.use(new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+        // Login
+        // Check if email exists
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return done(null, false, { message: "No user with this email." });
+        }
+        // Check password by comparing with DB
+        bcrypt.compare(password, user.password).then(match => {
+            if (match)
+                return done(null, user, { message: "Logged in Successfully." });
+            return done(null, false, { message: "Wrong username or password." });
+        }).catch(err => {
+            return done(null, false, { message: "Something went wrong." });
+        })
+    }))
+    // Storing user data in session
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    })
+    // GET user data from session
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, userData) => { // User.findOne({_id: id})
+            done(err, userData); // will get the user data in req.user
+        });
+    })
+}
+
+module.exports = init;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const MongoDbStore = require("connect-mongo")(session);
+const passport = require("passport");
 
 // Database Connection
 mongoose.connect('mongodb://localhost/salad', {
@@ -21,6 +22,13 @@ connection.once("open", () => {
 }).catch(err => {
     console.log("Connection failed. " + err);
 })
+
+// Passport Config (authentication middleware for Node.js)
+// require("./app/config/passport")(passport);
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Session Store
 const mongoStore = new MongoDbStore({
